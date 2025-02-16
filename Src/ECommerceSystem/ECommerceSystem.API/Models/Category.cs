879d7dd535019f1d1b7005d1c23f6ad4c161efe4
Md. Rifatul Islam <rifatuul@gmail.com>
2025-02-17 02:41:18 +0600

    feat(validation): add custom error messages for form validation

--- Src/ECommerceSystem/ECommerceSystem.API/Models/Category.cs
+++ Src/ECommerceSystem/ECommerceSystem.API/Models/Category.cs
@@ -13,7 +13,7 @@ namespace ECommerceSystem.API.Models
         public string Name { get; set; }
         [Required(ErrorMessage ="Display Order Must Be Required.")]
         [DisplayName("Display Order")]
-        [Range(1,100)]
+        [Range(1,100,ErrorMessage ="Display Order must be between 1-100")]
         public int DsiplayOrder { get; set; }
         public bool IsActive { get; set; }
         [Required(ErrorMessage ="Created Name Must Be Required")]
