e615e1faa46f21f1d25b5470976a238e04a15bb0
Md. Rifatul Islam <rifatuul@gmail.com>
2025-02-17 00:38:38 +0600

    refactor(api): inject ApplicationDbContext into CategoryController

--- Src/ECommerceSystem/ECommerceSystem.API/Controllers/CategoryController.cs
+++ Src/ECommerceSystem/ECommerceSystem.API/Controllers/CategoryController.cs
@@ -1,9 +1,16 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using ECommerceSystem.API.Data;
+using Microsoft.AspNetCore.Mvc;
 
 namespace ECommerceSystem.API.Controllers
 {
     public class CategoryController : Controller
     {
+        private readonly ApplicationDbContext _db;
+        public CategoryController(ApplicationDbContext db)
+        {
+            _db = db;
+        }
+
         public IActionResult Index()
         {
             return View();
