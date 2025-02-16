7c6f96509ec0f4c7a70b8b4d7729056b60ba5e3a
Md. Rifatul Islam <rifatuul@gmail.com>
2025-02-16 23:26:28 +0600

    feat(database): add category model for product classification

---
+++ Src/ECommerceSystem/ECommerceSystem.API/Models/Category.cs
@@ -0,0 +1,21 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ECommerceSystem.API.Models
+{
+    public class Category
+    {
+        [Key]
+        public int Id { get; set; }
+        [Required(ErrorMessage ="Category Must Be Required.")]
+        public string Name { get; set; }
+        [Required(ErrorMessage ="Display Order Must Be Required.")]
+        public int DsiplayOrder { get; set; }
+        public bool IsActive { get; set; }
+        [Required(ErrorMessage ="Created Name Must Be Required")]
+        public string CreatedBy { get; set; }
+        public DateTime CreatedDate { get; set; } = DateTime.Now;
+        public string? UpdateBy { get; set; }
+        public DateTime? UpdateDate { get; set; }
+
+    }
+}
