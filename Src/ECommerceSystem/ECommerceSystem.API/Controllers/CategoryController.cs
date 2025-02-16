4f63264723a1f2317565b8dc34bb986dbb9f1cf8
Md. Rifatul Islam <rifatuul@gmail.com>
2025-02-17 02:31:58 +0600

    feat(model): add MaxLength and Range validation for Category properties

--- Src/ECommerceSystem/ECommerceSystem.API/Controllers/CategoryController.cs
+++ Src/ECommerceSystem/ECommerceSystem.API/Controllers/CategoryController.cs
@@ -17,5 +17,16 @@ namespace ECommerceSystem.API.Controllers
             List<Category> categoryList = _db.Categories.ToList();
             return View(categoryList);
         }
+        public IActionResult Create()
+        {
+            return View();
+        }
+        [HttpPost]
+        public IActionResult Create(Category obj)
+        {
+            _db.Categories.Add(obj);
+            _db.SaveChanges();
+            return RedirectToAction("Index");
+        }
     }
 }
