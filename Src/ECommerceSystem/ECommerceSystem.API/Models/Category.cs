c593e30cbff4a8cdf8dfcb0f4568eaf87042eb3f
Md. Rifatul Islam <rifatuul@gmail.com>
2025-02-17 02:18:33 +0600

    chore(model): add DisplayName attributes for Category Name, Created By, and Display Order

--- Src/ECommerceSystem/ECommerceSystem.API/Models/Category.cs
+++ Src/ECommerceSystem/ECommerceSystem.API/Models/Category.cs
@@ -1,4 +1,5 @@
-﻿using System.ComponentModel.DataAnnotations;
+﻿using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
 
 namespace ECommerceSystem.API.Models
 {
@@ -7,11 +8,14 @@ namespace ECommerceSystem.API.Models
         [Key]
         public int Id { get; set; }
         [Required(ErrorMessage ="Category Must Be Required.")]
+        [DisplayName("Category Name")]
         public string Name { get; set; }
         [Required(ErrorMessage ="Display Order Must Be Required.")]
+        [DisplayName("Display Order")]
         public int DsiplayOrder { get; set; }
         public bool IsActive { get; set; }
         [Required(ErrorMessage ="Created Name Must Be Required")]
+        [DisplayName("Created By")]
         public string CreatedBy { get; set; }
         public DateTime CreatedDate { get; set; } = DateTime.Now;
         public string? UpdateBy { get; set; }
