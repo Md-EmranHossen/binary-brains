5fd202d64d4d3718e5c4a4cd28dd6f479890e646
MD. Famidul Islam Pranto <fipranto9001@gmail.com>
2025-02-17 10:45:22 +0600

    feat(core): add UnitOfWork interface and implementation

--- Src/ECommerceSystem/ECommerceSystem.DataAccess/Repository/UnitOfWork.cs
+++ Src/ECommerceSystem/ECommerceSystem.DataAccess/Repository/UnitOfWork.cs
@@ -1,27 +1,27 @@
 ﻿using ECommerceSystem.DataAccess.Repository.IRepository;
-using ECommerceSystem.Models;
 using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Linq.Expressions;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace ECommerceSystem.DataAccess.Repository
 {
-  public  class CategoryRepositroy : Repository<Category>, ICategoryRepository
+    public class UnitOfWork : IUnitOfWork
     {
+
         private ApplicationDbContext _db;
-        public CategoryRepositroy(ApplicationDbContext db) : base(db)
+        public ICategoryRepository Category { get; private set; }
+
+        public UnitOfWork(ApplicationDbContext db)
         {
             _db = db;
+            Category= new CategoryRepositroy(_db);
         }
 
-     
-
-        public void Update(Category obj)
+        public void Save()
         {
-            _db.Categories.Update(obj);
+            _db.SaveChanges();
         }
     }
 }
