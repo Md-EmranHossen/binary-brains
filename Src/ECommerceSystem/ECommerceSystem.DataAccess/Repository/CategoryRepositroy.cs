fa2a24a99afc4c616cc314bad2c98d8dede69d30
MD. Famidul Islam Pranto <fipranto9001@gmail.com>
2025-02-17 10:36:54 +0600

    feat(repository): add CategoryRepository class and interface

--- Src/ECommerceSystem/ECommerceSystem.DataAccess/Repository/CategoryRepositroy.cs
+++ Src/ECommerceSystem/ECommerceSystem.DataAccess/Repository/CategoryRepositroy.cs
@@ -1,12 +1,30 @@
-﻿using System;
+﻿using ECommerceSystem.DataAccess.Repository.IRepository;
+using ECommerceSystem.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Linq.Expressions;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace ECommerceSystem.DataAccess.Repository
 {
-    internal class CategoryRepositroy
+  public  class CategoryRepositroy : Repository<Category>, ICategoryRepository
     {
+        private ApplicationDbContext _db;
+        public CategoryRepositroy(ApplicationDbContext db) : base(db)
+        {
+            _db = db;
+        }
+
+        public void Save()
+        {
+            _db.SaveChanges();
+        }
+
+        public void Update(Category obj)
+        {
+            _db.Categories.Update(obj);
+        }
     }
 }
