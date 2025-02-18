983992994f075c326456ac57df47923a90b2d4a1
EmranHossen <emranhossen3075@gmail.com>
2025-02-18 22:09:31 +0600

    feat(identity): scaffold identity module for user authentication and authorization

--- Src/ECommerceSystem/ECommerceSystem.API/Program.cs
+++ Src/ECommerceSystem/ECommerceSystem.API/Program.cs
@@ -4,6 +4,7 @@ using ECommerceSystem.DataAccess;
 using ECommerceSystem.DataAccess.Repository;
 using ECommerceSystem.DataAccess.Repository.IRepository;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.AspNetCore.Identity;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -11,6 +12,8 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddControllersWithViews();
 builder.Services.AddDbContext<ApplicationDbContext>(options =>
 options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
+
+builder.Services.AddDefaultIdentity<IdentityUser>(options => options.SignIn.RequireConfirmedAccount = true).AddEntityFrameworkStores<ApplicationDbContext>();
 
 builder.Services.AddScoped<IUnitOfWork, UnitOfWork>();
 
--- Src/ECommerceSystem/ECommerceSystem.DataAccess/Data/ApplicationDbContext.cs
+++ Src/ECommerceSystem/ECommerceSystem.DataAccess/Data/ApplicationDbContext.cs
@@ -1,13 +1,21 @@
 ﻿using ECommerceSystem.Models;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
 
 namespace ECommerceSystem.DataAccess
 {
-    public class ApplicationDbContext:DbContext
+    public class ApplicationDbContext:IdentityDbContext<IdentityUser>
     {
         public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
         {
 
+        }
+
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            base.OnModelCreating(builder);
+           
         }
         public DbSet<Category> Categories { get; set; }
         public DbSet<Product> Products { get; set; }
