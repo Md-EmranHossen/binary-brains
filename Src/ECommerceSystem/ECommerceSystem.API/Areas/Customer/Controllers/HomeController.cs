66b2a288add12abb838e42edd190e35e50e62412
EmranHossen <emranhossen3075@gmail.com>
2025-02-18 13:23:39 +0600

    feat(ui): design home page

--- Src/ECommerceSystem/ECommerceSystem.API/Areas/Customer/Controllers/HomeController.cs
+++ Src/ECommerceSystem/ECommerceSystem.API/Areas/Customer/Controllers/HomeController.cs
@@ -11,10 +11,10 @@ namespace ECommerceSystem.API.Areas.Customer.Controllers
     {
         private readonly ILogger<HomeController> _logger;
         private readonly IUnitOfWork _unitOfWork;
-        public HomeController(ILogger<HomeController> logger)
+        public HomeController(ILogger<HomeController> logger, IUnitOfWork unitOfWork)
         {
             _logger = logger;
-            _unitOfWork = _unitOfWork;
+            _unitOfWork = unitOfWork;
         }
 
         public IActionResult Index()
