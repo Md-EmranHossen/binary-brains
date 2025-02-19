462350e955acf87a03128b3c19ccf87a69fdb89d
EmranHossen <emranhossen3075@gmail.com>
2025-02-19 13:06:44 +0600

    fix(auth): resolve bugs in login and registration

--- Src/ECommerceSystem/ECommerceSystem.API/Areas/Identity/Pages/Account/Register.cshtml.cs
+++ Src/ECommerceSystem/ECommerceSystem.API/Areas/Identity/Pages/Account/Register.cshtml.cs
@@ -110,6 +110,15 @@ namespace ECommerceWebApp.Areas.Identity.Pages.Account
             [ValidateNever]
             public IEnumerable<SelectListItem> RoleList { get; set; }
 
+            [Required]
+            public string? Name { get; set; }
+            public string? StreetAddress {  get; set; }
+            public string? City {  get; set; }
+            public string? State {  get; set; }
+            public string? PostCode {  get; set; }
+            public string? PhoneNumber {  get; set; }
+
+
         }
 
 
@@ -147,6 +156,16 @@ namespace ECommerceWebApp.Areas.Identity.Pages.Account
 
                 await _userStore.SetUserNameAsync(user, Input.Email, CancellationToken.None);
                 await _emailStore.SetEmailAsync(user, Input.Email, CancellationToken.None);
+
+
+                user.Name = Input.Name;
+                user.StreetAddress = Input.StreetAddress;
+                user.City = Input.City;
+                user.State = Input.State;
+                user.PostalCode = Input.PostCode;
+                user.PhoneNumber = Input.PhoneNumber;
+
+
                 var result = await _userManager.CreateAsync(user, Input.Password);
 
 
