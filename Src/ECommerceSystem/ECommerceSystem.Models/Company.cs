ea0c16a32677d8908b1e863fc6649c27385a2cf6
MD. Famidul Islam Pranto <fipranto9001@gmail.com>
2025-02-27 20:07:22 +0600

    feat(view): add Index view for company management

--- Src/ECommerceSystem/ECommerceSystem.Models/Company.cs
+++ Src/ECommerceSystem/ECommerceSystem.Models/Company.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
@@ -14,10 +15,12 @@ namespace ECommerceSystem.Models
         [Required]
         public string Name { get; set; }
 
+        [DisplayName("Street Address")]
         public string? StreetAddress { get; set; }
         public string? City { get; set; }
         public string? State { get; set; }
-        public string? PostalCode { get; set; } 
+        public string? PostalCode { get; set; }
+        [DisplayName("Phone No.")]
         public string? PhoneNumber { get; set; }
     }
 }
