f6ece07a3a8b055dc03b4303442658e6a9e678b3
MD. Famidul Islam Pranto <fipranto9001@gmail.com>
2025-02-27 19:36:54 +0600

    feat(model): add Company model

---
+++ Src/ECommerceSystem/ECommerceSystem.DataAccess/Repository/CompanyRepository.cs
@@ -0,0 +1,27 @@
+﻿using ECommerceSystem.DataAccess.Repository.IRepository;
+using ECommerceSystem.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ECommerceSystem.DataAccess.Repository
+{
+  public  class CompanyRepository : Repository<Company>, ICompanyRepositroy
+    {
+        private ApplicationDbContext _db;
+        public CompanyRepository(ApplicationDbContext db) : base(db) 
+        {
+            _db = db;
+        }
+
+     
+
+        public void Update(Company obj)
+        {
+            _db.Categories.Update(obj);
+        }
+    }
+}
