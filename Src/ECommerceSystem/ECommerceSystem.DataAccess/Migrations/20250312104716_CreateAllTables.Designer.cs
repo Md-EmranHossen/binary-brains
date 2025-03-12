ba2e0de23b06e59b0eb51b12e2d867a54fe24e1b
EmranHossen <emranhossen3075@gmail.com>
2025-03-12 16:50:36 +0600

    Add: Add Some files

--- Src/ECommerceSystem/ECommerceSystem.DataAccess/Migrations/20250312104716_CreateAllTables.Designer.cs
+++ Src/ECommerceSystem/ECommerceSystem.DataAccess/Migrations/20250312104716_CreateAllTables.Designer.cs
@@ -12,8 +12,8 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace ECommerceSystem.DataAccess.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    [Migration("20250227162720_AddingCompanyInUser")]
-    partial class AddingCompanyInUser
+    [Migration("20250312104716_CreateAllTables")]
+    partial class CreateAllTables
     {
         /// <inheritdoc />
         protected override void BuildTargetModel(ModelBuilder modelBuilder)
