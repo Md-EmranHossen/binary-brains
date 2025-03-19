270a02eafc71cc80acbc34d40d136562323c619b
Md. Rifatul Islam <rifatuul@gmail.com>
2025-03-20 02:02:37 +0600

    feat(repository): Add repositories for OrderHeader and OrderDetail

---
+++ Src/ECommerceSystem/ECommerceSystem.DataAccess/Repository/OrderDetailRepositroy.cs
@@ -0,0 +1,27 @@
+﻿using ECommerceSystem.DataAccess.Repository.IRepository;
+using ECommerceSystem.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ECommerceSystem.DataAccess.Repository
+{
+    public class OrderDetailRepositroy : Repository<OrderDetail>, IOrderDetailRepository
+    {
+        private ApplicationDbContext _db;
+        public OrderDetailRepositroy(ApplicationDbContext db) : base(db)
+        {
+            _db = db;
+        }
+
+
+
+        public void Update(OrderDetail obj)
+        {
+            _db.OrderDetails.Update(obj);
+        }
+    }
+}
---
+++ Src/ECommerceSystem/ECommerceSystem.DataAccess/Repository/OrderHeaderRepositroy.cs
@@ -0,0 +1,27 @@
+﻿using ECommerceSystem.DataAccess.Repository.IRepository;
+using ECommerceSystem.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ECommerceSystem.DataAccess.Repository
+{
+    public class OrderHeaderRepositroy : Repository<OrderHeader>, IOrderHeaderRepository
+    {
+        private ApplicationDbContext _db;
+        public OrderHeaderRepositroy(ApplicationDbContext db) : base(db)
+        {
+            _db = db;
+        }
+
+
+
+        public void Update(OrderHeader obj)
+        {
+            _db.OrderHeaders.Update(obj);
+        }
+    }
+}
