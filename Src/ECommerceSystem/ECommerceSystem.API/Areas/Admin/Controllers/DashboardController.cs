fe9188857358650127c9c0d65fa1d406b2964e81
EmranHossen <emranhossen3075@gmail.com>
2025-04-13 02:53:02 +0600

    feat(auth): add role-based sidebar visibility for admin users

---
+++ Src/ECommerceSystem/ECommerceSystem.API/Areas/Admin/Controllers/DashboardController.cs
@@ -0,0 +1,16 @@
+﻿using ECommerceSystem.Models;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ECommerceWebApp.Areas.Admin.Controllers
+{
+    [Area("Admin")]
+    [Authorize(Roles = SD.Role_Admin)]
+    public class DashboardController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
