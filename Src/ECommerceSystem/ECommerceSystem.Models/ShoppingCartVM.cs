2f3bda80d6a8762edf89f26e75a6bfc91dd06fc7
MD. Famidul Islam Pranto <fipranto9001@gmail.com>
2025-04-23 21:24:50 +0600

    feat(test):add xunit test for navManager

--- Src/ECommerceSystem/ECommerceSystem.Models/ShoppingCartVM.cs
+++ Src/ECommerceSystem/ECommerceSystem.Models/ShoppingCartVM.cs
@@ -1,7 +1,9 @@
-﻿using System.Collections.Generic;
+﻿using Microsoft.AspNetCore.Mvc.ModelBinding.Validation;
+using System.Collections.Generic;
 
 namespace ECommerceSystem.Models
 {
+    [ValidateNever]
     public class ShoppingCartVM
     {
         public IEnumerable<ShoppingCart> ShoppingCartList { get; set; } = Enumerable.Empty<ShoppingCart>();
